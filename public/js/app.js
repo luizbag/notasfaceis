d46c597b2bff49ad92823d0864a7998066712d2b
Luiz Gavioli <luizbag@gmail.com>
2015-06-20 09:46:44 -0300

    Começando as rotas e o login

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,17 @@ angular.module('app', ['app.controllers', 'ui.router'])
       .state('home', {
         url: "/home",
         templateUrl: "/html/home.html"
+      })
+      .state('entrar', {
+        url: "/entrar",
+        templateUrl: "/html/entrar.html"
+      })
+      .state('cadastrar', {
+        url: "/cadastrar",
+        templateUrl: "/html/cadastrar.html"
+      })
+      .state('painel', {
+        url: "/painel",
+        templateUrl: "/html/painel.html"
       });
   });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,6 @@
-angular.module('app.controllers', []);
+angular.module('app.controllers', [])
+  .controller('LoginController', ['$scope','$state', function($scope, $state) {
+    $scope.login = function(email, senha) {
+      $state.go('painel');
+    }
+  }]);
