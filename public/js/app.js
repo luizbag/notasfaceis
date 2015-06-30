914e23cdc166fea026aa9f93eb0a7e57014dc672
Luiz Gavioli <luizbag@gmail.com>
2015-06-30 09:42:44 -0300

    Login funcionando no browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,42 @@
-angular.module('app', ['app.controllers', 'ui.router'])
-  .config(function($stateProvider, $urlRouterProvider) {
+angular.module('app', ['app.controllers', 'ui.router', 'LocalStorageModule'])
+  .config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
+    localStorageServiceProvider
+      .setPrefix('notasfaceis')
+      .setStorageType('sessionStorage')
+      .setNotify(true, true);
+
     $urlRouterProvider.otherwise("/home");
 
     $stateProvider
       .state('home', {
         url: "/home",
-        templateUrl: "/html/home.html"
+        templateUrl: "/html/home.html",
+        data: {
+          requireLogin: false
+        }
       })
       .state('entrar', {
         url: "/entrar",
-        templateUrl: "/html/entrar.html"
+        templateUrl: "/html/entrar.html",
+        data: {
+          requireLogin: false
+        }
       })
       .state('cadastrar', {
         url: "/cadastrar",
-        templateUrl: "/html/cadastrar.html"
+        templateUrl: "/html/cadastrar.html",
+        data: {
+          requireLogin: false
+        }
       });
+  })
+  .run(function($rootScope, $state, localStorageService) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+      var requireLogin = toState.data.requireLogin;
+      var token = localStorageService.get('token');
+      if(requireLogin && !token) {
+        event.preventDefault();
+        $state.go('entrar');
+      }
+    });
   });
---
+++ public/js/services.js
@@ -0,0 +1,20 @@
+angular.module('app.services', ['ngResource'])
+  .service('LoginService', ['$http', function($http) {
+    this.login = function(email, password, callback) {
+      $http.post('/login', {"email": email, "password": password}).
+        success(function(data, status, headers, config) {
+          callback(data);
+        }).
+        error(function(data, status, headers, config) {
+          var token = "Unauthorized";
+          callback(token);
+        });
+    };
+
+    this.register = function(email, password, callback) {
+      $http.post('/login/register', {"email": email, "password": password}).
+        success(function(data, status, headers, config) {
+          callback(data);
+        });
+    };
+  }]);
